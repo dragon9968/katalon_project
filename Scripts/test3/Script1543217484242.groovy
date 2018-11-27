e5cf00d2a8bb72ebf7daf363c74ada020f9b5980
longnguyen <longnguyen@LONGNGUYEN.vodc.lan>
2018-11-27 15:06:42 +0700

    user2 add more code into testgit

--- Scripts/test3/Script1543217484242.groovy
+++ Scripts/test3/Script1543217484242.groovy
@@ -13,7 +13,3 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.acceptAlert()
-
-WebUI.authenticate('', '', '', 0)
-
