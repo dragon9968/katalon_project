a9a376c3f8c39b958ed6a10b3019372a5eae88ec
longnguyen <longnguyen@LONGNGUYEN.vodc.lan>
2018-11-29 16:52:53 +0700

    update

--- Object Repository/New Test Object.rs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
-   <description></description>
-   <name>New Test Object</name>
-   <tag></tag>
-   <elementGuidId>8302058b-c196-4330-b599-e95d70356b61</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
-</WebElementEntity>
